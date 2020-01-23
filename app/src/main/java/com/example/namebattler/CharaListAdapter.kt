045ce94785302289048bca7d037636b8adb68ba5
m_oota <m_oota@freestyles.jp>
2020-01-23 23:16:35 +0900

    [2020/01/23]@2.6 キャラクター一覧画面_RecyclerView 実装（作業途中）

---
+++ app/src/main/java/com/example/namebattler/CharaListAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.namebattler
+
+
+import androidx.recyclerview.widget.RecyclerView
+
+class CharaListAdapter(private val data: IntArray): RecyclerView.Adapter<CharaListAdapter.CharaListViewHolder>(){
+
+    CharaListViewHolder(view : view) : RecyclerView.ViewHolder
+}
--- app/src/main/java/com/example/namebattler/CharacterListActivity.kt
+++ app/src/main/java/com/example/namebattler/CharacterListActivity.kt
@@ -2,15 +2,29 @@ package com.example.namebattler
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+
 import androidx.recyclerview.widget.RecyclerView
 
 class CharacterListActivity : AppCompatActivity() {
-    private  var mRecyclerView : RecyclerView? = null
+    // RecyclerView 本体、および、LayoutManager と Adapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+    private lateinit var adapter: RecyclerView.Adapter<*>
 
+    // Adapter にセットするデータ (1～100)
+    private val data = IntArray(100) { it + 1 }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_character_list)
+        setContentView(R.layout.list_view)
+
+        layoutManager = LinearLayoutManager(this)
+        adapter = CharaListAdapter(data)
+        recyclerView = findViewById<RecyclerView>(R.id.list_view).also{
+            it.layoutManager = layoutManager
+            it.adapter = adapter
+        }
 
     }
 }
